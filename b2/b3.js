78931dfd48a44e2d223385285ed0eec5d1309628
Luong Nguyen <luongnguyen.nd2108@gmail.com>
2023-08-06 10:10:18 +0700

    first commit

---
+++ b2/b3.js
@@ -0,0 +1,9 @@
+let a = 5;
+let b = 5;
+// const x = 5;
+// x = 6;
+
+console.log(`Hello"  '''""" world from javascrip:`, a == b)
+
+let x = 5, y = 6, z = 7;
+console.log("Test string ", 'x= ', x, ', y= ', y,', z= ', z)
